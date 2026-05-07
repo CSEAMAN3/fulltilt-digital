ae3f4f94e735ce65ac362866367c5fe3d0d4d877
Christopher Seaman <christopherseaman1983@gmail.com>
2026-05-07 12:24:55 +0100

    update service slider heading

--- src/components/HomeServicesSection.tsx
+++ src/components/HomeServicesSection.tsx
@@ -4,8 +4,9 @@ export default function HomeServicesSection() {
   return (
     <div className="max-w-300 mx-auto px-8 py-16 sm:py-24">
       <div className="">
-        <h2 className="font-bold text-4xl sm:text-5xl mb-4 tracking-tight">
-          Made to work
+        <h2 className="font-bold text-4xl mb-4 tracking-tight text-balance">
+          <span className="block text-2xl mb-2">Made to work</span>
+          Designed to generate enquiries
         </h2>
         <p className="font-light max-w-[60ch]">
           Everything we offer is designed to support your growth. From websites
