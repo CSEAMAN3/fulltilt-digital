61d70d964acc7474a314e1b53092d638f30fdff2
Christopher Seaman <christopherseaman1983@gmail.com>
2026-05-08 21:07:58 +0100

    update service pages

--- src/app/services/brand-identity/page.tsx
+++ src/app/services/brand-identity/page.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 export default function BrandIdentityPage() {
   return (
-    <div className="min-h-screen grid place-content-center">
-      <div>This page is under construction</div>
-    </div>
+    <main className="">
+      {/* add structured data regards schemas */}
+      <div className="bg-linear-to-b from-brand-main to-transparent">
+        <div className="text-center grid place-content-center px-8 pt-60 pb-40">
+          <h1 className="font-bold text-balance mb-4 max-w-[24ch] text-[40px] sm:text-[60px] leading-none tracking-tight">
+            Brand + Identity
+          </h1>
+          <p className="font-light text-balance mb-8 max-w-[60ch] mx-auto">
+            We create clear, confident brand identities that help businesses
+            stand apart from competitors and build trust with the people they
+            want to reach.
+          </p>
+          <Link
+            href={"/contact"}
+            className="font-bold w-fit mx-auto mb-8 flex gap-2 group bg-brand-main py-4 px-8 rounded-full hover:bg-brand-main-dark transition-all duration-300"
+          >
+            Lets create something great
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/images/arrow.svg"
+              alt=""
+              width={10}
+              style={{ width: "20px", height: "auto" }}
+              className="group-hover:-rotate-45 transition-all duration-300"
+            />
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
--- src/app/services/video-photography/page.tsx
+++ src/app/services/video-photography/page.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 export default function VideoPhotographyPage() {
   return (
-    <div className="min-h-screen grid place-content-center">
-      <div>This page is under construction</div>
-    </div>
+    <main className="">
+      {/* add structured data regards schemas */}
+      <div className="bg-linear-to-b from-brand-main to-transparent">
+        <div className="text-center grid place-content-center px-8 pt-60 pb-40">
+          <h1 className="font-bold text-balance mb-4 max-w-[24ch] text-[40px] sm:text-[60px] leading-none tracking-tight">
+            Video + Photography
+          </h1>
+          <p className="font-light text-balance mb-8 max-w-[60ch] mx-auto">
+            We create professional photography and video that showcase your
+            business clearly, build trust with potential customers and bring
+            your brand to life across your website, social media and marketing.
+          </p>
+          <Link
+            href={"/contact"}
+            className="font-bold w-fit mx-auto mb-8 flex gap-2 group bg-brand-main py-4 px-8 rounded-full hover:bg-brand-main-dark transition-all duration-300"
+          >
+            Lets show your audience
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/images/arrow.svg"
+              alt=""
+              width={10}
+              style={{ width: "20px", height: "auto" }}
+              className="group-hover:-rotate-45 transition-all duration-300"
+            />
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
