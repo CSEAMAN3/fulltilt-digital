4cc7b8b5e42cb7a368c22471459211ddf57fe9b9
Christopher Seaman <christopherseaman1983@gmail.com>
2026-05-30 09:02:25 +0100

    update logo header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <Link href="/" className="block w-fit">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src="/images/logo-black.svg"
+            src="/images/logo.svg"
             alt="fulltilt digital logo"
             className="w-50 h-auto"
           />
