d31159d9dadfebadcf785a1480f5b87b2f8b9320
Christopher Seaman <christopherseaman1983@gmail.com>
2026-06-04 14:41:58 +0100

    update wording

--- src/lib/pricing.ts
+++ src/lib/pricing.ts
@@ -176,7 +176,7 @@ export const pricing: Pricing[] = [
     headingSmall: "Capture Attention",
     headingLarge: "Photography",
     paragraph:
-      "Professional photography designed to showcase your business, your people and the quality of your work. Perfect for businesses looking to replace stock imagery with authentic visuals that build trust and strengthen their online presence.",
+      "Professional photography created to showcase your business, your people and the quality of your work. Perfect for businesses looking to replace stock imagery with authentic visuals that build trust and strengthen their online presence.",
     priceLarge: "£200",
     priceSmall: "+ VAT",
     monthlyCarePlan: false,
